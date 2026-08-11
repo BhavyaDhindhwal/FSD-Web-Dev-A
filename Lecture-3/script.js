1664c06a046210187ecb743a5c8f38e7bf62a3ff
BhavyaDhindhwal <bkdhindhwal143@gmail.com>
2026-08-11 10:50:03 +0530

    first commit

---
+++ Lecture-3/script.js
@@ -0,0 +1,14 @@
+console.log("Hello World")
+//const math = require('./math.js');
+//const os = require('os');
+//const express = require('express');
+import {add, sub} from './math.js';
+
+//console.log(math.add(10, 20));
+//console.log(math.sub(10, 20));
+//console.log(os.platform());
+//console.log(os.arch());
+//console.log(os.cpus().length);
+//console.log(os.freemem()/(1024*1024*1024) +" GB");
+console.log(add(10, 20));
+console.log(sub(10, 20));
