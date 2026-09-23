181d3601eef363fc497afbba90f26b7bd192b283
BhavyaDhindhwal <bkdhindhwal143@gmail.com>
2026-09-23 09:40:24 +0530

    Initial project upload

--- Lecture-21/students.mongodb.js
+++ Lecture-21/students.mongodb.js
@@ -5,11 +5,12 @@ db.createCollection("students");
 // {rollNo:1, name:"Bhavya",section:"FSD-A",marks:80}
 // )
 
-// db.students.insertMany([
+//db.students.insertMany([
 //     {rollNo:2, name:"Ananya",section:"FSD-A",marks:90},
 //     {rollNo:3, name:"Rohit",section:"FSD-A",marks:70},
 //     {rollNo:4, name:"Sakshi",section:"FSD-A",marks:85},
 //     {rollNo:5, name:"Amit",section:"FSD-A",marks:75}
+//       {rollNo:5, name:"Amit",section:"FSD-A",marks:75}
 // ])
 
 // db.students.insert(
@@ -17,7 +18,7 @@ db.createCollection("students");
 // )
 
 // db.students.findOne();
-// db.students.find();
+//db.students.find();
 
 // db.students.find({section:"FSD-B"});
 
@@ -26,4 +27,7 @@ db.createCollection("students");
 //db.students.updateOne({name:"Rohit"}, {$set: {marks: 75}})
 
 
-db.students.updateMany({section:"FSD-A"}, {$set: {section: "FSD-B"}})
+//db.students.updateMany({section:"FSD-A"}, {$set: {section: "FSD-B"}})
+//db.students.deleteOne({rollNo: 6});
+//db.students.deleteMany({section: "FSD-B"});
+db.students.findOne();
